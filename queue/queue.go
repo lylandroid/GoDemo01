a1aa964862af830f6e640ecd2941feb7c344c899
yilan.luo@zhangmen.com <yilan.luo@zhangmen.com>
2019-07-17 19:20:13 +0800

    godoc使用 & 注释

--- queue/queue.go
+++ queue/queue.go
@@ -1,16 +1,21 @@
 package queue
 
+// An FIFO queue.
 type Queue []int
 
+// Pushes the element into the queue.
 func (q *Queue) Push(value int) {
 	*q = append(*q, value)
 }
 
+//Pops element from head
 func (q *Queue) Pop() int {
 	head := (*q)[0]
 	*q = (*q)[1:]
 	return head
 }
+
+//Returns if the queue is emptr or not.
 func (q *Queue) IsEmpty() bool {
 	return len(*q) == 0
 }
