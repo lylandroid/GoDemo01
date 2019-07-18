798dabd01e271b2cc000d5624b8c943c126b3693
yilan.luo@zhangmen.com <yilan.luo@zhangmen.com>
2019-07-18 15:50:02 +0800

    goroutine交出控制权

--- goroutine/goroutine.go
+++ goroutine/goroutine.go
@@ -1,18 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"runtime"
 	"time"
+	"fmt"
 )
 
+const UINT_MAX = 1024
+
 func main() {
-	for i := 0; i < 1000; i++ {
+	//var mpas = make(map[int]string)
+	fmt.Println(UINT_MAX)
+	var arr [UINT_MAX] int
+	for i := 0; i < UINT_MAX; i++ {
 		go func(i int) {
 			for {
-				fmt.Printf("Hello from goroutine  i=%d \n", i)
+				arr[i] = arr[i] + 1
+				runtime.Gosched() //交出控制权
+				//mpas[i] = "Hello from goroutine  i=" + string(i)
+				//fmt.Printf("Hello from goroutine  i=%d \n", i)
 			}
 		}(i)
 	}
-	time.Sleep(time.Millisecond)
+	time.Sleep(time.Millisecond * 1000)
+	/*for k, v := range arr {
+		fmt.Println(k, v)
+	}*/
+	fmt.Println(arr)
 
 }
