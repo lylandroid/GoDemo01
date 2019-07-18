fe9a0587fdd6fc6e9ca85ed1d0c0fcfb7b698dd9
yilan.luo@zhangmen.com <yilan.luo@zhangmen.com>
2019-07-18 17:19:36 +0800

    channel demo2(函数返回chan)

--- channel/channel.go
+++ channel/channel.go
@@ -5,19 +5,40 @@ import (
 	"fmt"
 )
 
-func worker(id int, c chan int) {
-	for {
-		n := <-c
-		fmt.Println(id, n)
-	}
+func createWorker(id int) chan int {
+	c := make(chan int)
+	go func() {
+		for {
+			fmt.Println(id, (string(<-c)))
+		}
+	}()
+	return c
 }
 
 func chanDemo() {
-	c := make(chan int)
-	go worker(0, c)
-	c <- 1
-	c <- 2
-	time.Sleep(time.Millisecond)
+	var channels [10] chan int
+	for i := 0; i < 10; i++ {
+		channels[i] = createWorker(i)
+
+	}
+	for i := 0; i < 10; i++ {
+		channels[i] <- 'a' + i
+	}
+	for i := 0; i < 10; i++ {
+		channels[i] <- 'A' + i
+	}
+	time.Sleep(time.Millisecond * 10)
+	//Output
+	//0 97
+	//4 101
+	//1 98
+	//2 99
+	//3 100
+	//6 103
+	//5 102
+	//7 104
+	//8 105
+	//9 106
 }
 
 func main() {
