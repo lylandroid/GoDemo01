e7b4dba9718d4a38fb3db1e0e1d81e416e7e0594
lylAndroid <lylAndroid@163.com>
2019-07-18 23:08:31 +0800

    channel & WaitGroup等待

--- channel/channel2.go
+++ channel/channel2.go
@@ -1,30 +1,32 @@
 package main
 
 import (
-	"time"
 	"fmt"
+	"sync"
 )
 
 type Worker struct {
 	in   chan int
-	done chan bool
+	done func()
 }
 
 func worker2(id int, worker Worker) {
 	func() {
 		for v := range worker.in {
-			fmt.Println(id, v)
-			worker.done <- true
+			fmt.Println(id, string(v))
+			worker.done()
 		}
 	}()
 }
 
 //外部只能发数据：chan<- int，
 // 外部只能收数据：<-chan int
-func createWorker2(id int) Worker {
+func createWorker2(id int, wg *sync.WaitGroup) Worker {
 	w := Worker{
-		in:   make(chan int),
-		done: make(chan bool),
+		in: make(chan int),
+		done: func() {
+			wg.Done()
+		},
 	}
 	go worker2(id, w)
 	return w
@@ -32,30 +34,20 @@ func createWorker2(id int) Worker {
 
 //通道demo
 func chanDemo2() {
-	var channels [10] Worker
+	var wg sync.WaitGroup
+	var workers [10]Worker
 	for i := 0; i < 10; i++ {
-		channels[i] = createWorker2(i)
+		workers[i] = createWorker2(i, &wg)
 	}
-	for i := 0; i < 10; i++ {
-		channels[i].in <- 'a' + i
-		<-channels[i].done
+	wg.Add(20)
+	for i, w := range workers {
+		w.in <- 'a' + i
 	}
-	for i := 0; i < 10; i++ {
-		channels[i].in <- 'A' + i
-		<-channels[i].done
+	for i, w := range workers {
+		w.in <- 'A' + i
 	}
-	time.Sleep(time.Millisecond * 10)
-	//Output
-	//0 97
-	//4 101
-	//1 98
-	//2 99
-	//3 100
-	//6 103
-	//5 102
-	//7 104
-	//8 105
-	//9 106
+
+	wg.Wait()
 }
 
 func main() {
