b73ccb5fd73ed0e726aedda53221db28d8f23b6a
yilan.luo@zhangmen.com <yilan.luo@zhangmen.com>
2019-08-15 14:52:34 +0800

    html路径修改为相对路径

--- crawler/frontend/starter.go
+++ crawler/frontend/starter.go
@@ -19,7 +19,7 @@ func main() {
 }
 
 func startServer()  {
-	htmlPath := "E:/project_workspace/idea/go/demo1/crawler/frontend/view/index.html"
+	htmlPath := "./crawler/frontend/view/index.html"
 	http.Handle("/search", controller.CreateSearchResultHandler(htmlPath))
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
